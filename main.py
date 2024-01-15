436b26f1537dcb9e8ebb27b9c7895799d125012c
= <=>
2024-01-15 18:50:15 +0100

    Added relationship statements generation

--- main.py
+++ main.py
@@ -1,6 +1,76 @@
 import random
 import math
+import openpyxl
 
+
+def get_value_from_cell_reference(ws, cell_reference):
+    return ws[cell_reference].value
+
+def print_statement(i, liked_recipe, custom_user, liked, type):
+    if type == "SQL":
+        if liked:
+            return f"('{i}', UNHEX(REPLACE('{liked_recipe}', '-', '')), UNHEX(REPLACE('{custom_user}', '-', ''))) "
+        else:
+            return f"(UNHEX(REPLACE('{liked_recipe}', '-', '')), UNHEX(REPLACE('{custom_user}', '-', ''))) "
+    if type == "Cypher":
+        return "{{recipeId: '{}', userId: '{}'}} ".format(liked_recipe, custom_user)
+
+def generate_sql_inserts_from_excel(excel_file, liked_recipe_file, custom_user_file, output_file, liked, type):
+    print("Generating sql insert: \n")
+    wb = openpyxl.load_workbook(excel_file)
+
+    start = "INSERT INTO `recipe-forum`.`custom_users_liked_recipes` (`id`, `liked_recipe_id`, `custom_user_id`) VALUES " if liked else "INSERT INTO `recipe-forum`.`custom_users_disliked_recipes` (`disliked_recipe_id`, `custom_user_id`) VALUES "
+    out = open(output_file, 'w')
+    out.write(start)
+
+    with open(liked_recipe_file, 'r') as lr, open(custom_user_file, 'r') as cu:
+        for i, (liked_recipe_cell, custom_user_cell) in enumerate(zip(lr, cu), start=1):
+            print(i)
+            liked_recipe_sheet, liked_recipe_cell = liked_recipe_cell.strip().lstrip('=').split('!')
+            custom_user_sheet, custom_user_cell = custom_user_cell.strip().lstrip('=').split('!')
+
+            liked_recipe = get_value_from_cell_reference(wb[liked_recipe_sheet], liked_recipe_cell)
+            custom_user = get_value_from_cell_reference(wb[custom_user_sheet], custom_user_cell)
+
+            if liked_recipe and custom_user:  # Skip if any value is None
+                out.write(print_statement(i, liked_recipe, custom_user, liked, type))
+
+            if i % 10000 == 0:
+                out.write("; \n")
+                out.write(start)
+            else:
+                out.write(", ")
+
+def generate_cypher_inserts_from_excel(excel_file, liked_recipe_file, custom_user_file, output_file, liked, type):
+    print("Generating cypher insert: \n")
+    wb = openpyxl.load_workbook(excel_file)
+
+    start = "WITH ["
+    endLiked = "] AS data UNWIND data AS row MATCH (r:Recipe {id: row.recipeId}), (u:CustomUser {id: row.userId}) CREATE (u)-[:LIKED]->(r); \n"
+    endDisliked = "] AS data UNWIND data AS row MATCH (r:Recipe {id: row.recipeId}), (u:CustomUser {id: row.userId}) CREATE (u)-[:DISLIKED]->(r); \n"
+    out = open(output_file, 'w')
+    out.write(start)
+
+    with open(liked_recipe_file, 'r') as lr, open(custom_user_file, 'r') as cu:
+        for i, (liked_recipe_cell, custom_user_cell) in enumerate(zip(lr, cu), start=1):
+            print(i)
+            liked_recipe_sheet, liked_recipe_cell = liked_recipe_cell.strip().lstrip('=').split('!')
+            custom_user_sheet, custom_user_cell = custom_user_cell.strip().lstrip('=').split('!')
+
+            liked_recipe = get_value_from_cell_reference(wb[liked_recipe_sheet], liked_recipe_cell)
+            custom_user = get_value_from_cell_reference(wb[custom_user_sheet], custom_user_cell)
+
+            if liked_recipe and custom_user:  # Skip if any value is None
+                out.write(print_statement(i, liked_recipe, custom_user, liked, type))
+
+            if i % 10000 == 0:
+                if liked:
+                    out.write(endLiked)
+                else:
+                    out.write(endDisliked)
+                out.write(start)
+            else:
+                out.write(", ")
 
 def print_sql_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size):
 
@@ -81,21 +151,6 @@ def print_sql_relations(user_size, category_size, recipe_size, comment_size, pro
     for i in range(order_size):
         order_user += user + str(random.randint(2, user_size)) + "\n"
 
-    print("Generating Liked and Disliked Recipes relations")
-
-    for i in range(2, math.floor(recipe_size/2)):
-        print("Generating relations for {} recipe of {}".format(i, recipe_size/2))
-        no_users_liking = random.randint(0, math.floor(user_size/4))
-        liking_users = random.sample(range(2, user_size), no_users_liking)
-        no_users_disliking = random.randint(0, math.floor(user_size/12))
-        disliking_users = [x for x in random.sample(range(2, user_size), no_users_disliking) if x not in liking_users]
-        for j in range(len(liking_users)):
-            liked_recipe_liked_recipe += recipe + str(i) + "\n"
-            liked_recipe_custom_user += user + str(liking_users[j]) + "\n"
-        for j in range(len(disliking_users)):
-            disliked_recipe_disliked_recipe += recipe + str(i) + "\n"
-            disliked_recipe_custom_user += user + str(disliking_users[j]) + "\n"
-
     print("Writing files")
 
     recipe_category_file = open("sql/recipeCategory.txt", "w")
@@ -130,21 +185,28 @@ def print_sql_relations(user_size, category_size, recipe_size, comment_size, pro
     order_user_file.write(order_user)
     order_user_file.close()
 
-    liked_recipe_liked_recipe_file = open("sql/likedRecipeLikedRecipe.txt", "w")
-    liked_recipe_liked_recipe_file.write(liked_recipe_liked_recipe)
-    liked_recipe_liked_recipe_file.close()
+    print("Generating Liked and Disliked Recipes relations")
 
-    liked_recipe_custom_user_file = open("sql/likedRecipeCustomUser.txt", "w")
-    liked_recipe_custom_user_file.write(liked_recipe_custom_user)
-    liked_recipe_custom_user_file.close()
+    with open("sql/likedRecipeLikedRecipe.txt", "w") as liked_recipe_liked_recipe_file, \
+            open("sql/likedRecipeCustomUser.txt", "w") as liked_recipe_custom_user_file, \
+            open("sql/dislikedRecipeDislikedRecipe.txt", "w") as disliked_recipe_disliked_recipe_file, \
+            open("sql/dislikedRecipeCustomUser.txt", "w") as disliked_recipe_custom_user_file:
 
-    disliked_recipe_disliked_recipe_file = open("sql/dislikedRecipeDislikedRecipe.txt", "w")
-    disliked_recipe_disliked_recipe_file.write(disliked_recipe_disliked_recipe)
-    disliked_recipe_disliked_recipe_file.close()
+        for i in range(2, math.floor(recipe_size / 2)):
+            print(f"Generating relations for {i} recipe of {recipe_size / 2}")
+            no_users_liking = random.randint(0, math.floor(user_size / 4))
+            liking_users = random.sample(range(2, user_size), no_users_liking)
+            no_users_disliking = random.randint(0, math.floor(user_size / 12))
+            disliking_users = [x for x in random.sample(range(2, user_size), no_users_disliking) if
+                               x not in liking_users]
 
-    disliked_recipe_custom_user_file = open("sql/dislikedRecipeCustomUser.txt", "w")
-    disliked_recipe_custom_user_file.write(disliked_recipe_custom_user)
-    disliked_recipe_custom_user_file.close()
+            for custom_user in liking_users:
+                liked_recipe_liked_recipe_file.write(f"{recipe}{i}\n")
+                liked_recipe_custom_user_file.write(f"{user}{custom_user}\n")
+
+            for custom_user in disliking_users:
+                disliked_recipe_disliked_recipe_file.write(f"{recipe}{i}\n")
+                disliked_recipe_custom_user_file.write(f"{user}{custom_user}\n")
 
 
 def print_cypher_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size):
@@ -304,16 +366,28 @@ def print_cypher_relations(user_size, category_size, recipe_size, comment_size, 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    user_size = 10000
-    category_size = 41
-    recipe_size = 100000
-    comment_size = 500000
+    user_size = 2500
+    category_size = 40
+    recipe_size = 25000
+    comment_size = 125000
     product_category_size = 15
-    product_size = 200
-    line_item_size = 3000
-    order_size = 1500
+    product_size = 100
+    line_item_size = 1000
+    order_size = 500
 
-    print_sql_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size)
-    print_cypher_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size)
+    # print_sql_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size)
+    # print_cypher_relations(user_size, category_size, recipe_size, comment_size, product_category_size, product_size, line_item_size, order_size)
+
+    # generate_sql_inserts_from_excel('SQL.xlsx', 'sql/likedRecipeLikedRecipe.txt', 'sql/likedRecipeCustomUser.txt',
+    #                                 'sql/insert_liked_statements.sql', True, "SQL")
+    generate_sql_inserts_from_excel('SQL.xlsx', 'sql/dislikedRecipeDislikedRecipe.txt',
+                                    'sql/dislikedRecipeCustomUser.txt',
+                                    'sql/insert_disliked_statements.sql', False, "SQL")
+    #
+    # generate_cypher_inserts_from_excel('Cypher25k.xlsx', 'cypher/likedRecipeLikedRecipe.txt', 'cypher/likedRecipeCustomUser.txt',
+    #                                 'cypher/insert_liked_statements.cypher', True, "Cypher")
+    # generate_cypher_inserts_from_excel('Cypher25k.xlsx', 'cypher/dislikedRecipeDislikedRecipe.txt',
+    #                                 'cypher/dislikedRecipeCustomUser.txt',
+    #                                 'cypher/insert_dislike_statements.cypher', False, "Cypher")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
